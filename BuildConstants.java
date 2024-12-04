6d58a3fd25d59054e3b8e42941f60a1dc2ec75cd
Audrey <89819620+audreypj@users.noreply.github.com>
2024-12-03 16:57:11 -0800

    Add files via upload

---
+++ BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024-offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "7c211afb2cc829c1dd78c4704d65b97fb64616d8";
+  public static final String GIT_DATE = "2024-11-18 00:26:33 PST";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2024-11-21 16:27:34 PST";
+  public static final long BUILD_UNIX_TIME = 1732235254679L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
